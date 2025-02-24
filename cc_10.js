3d006a56f26f49484aa4db6f6482e3c20b8a20cb
santiago lopez <slopezarias@usf.edu>
2025-02-23 23:10:23 -0500

    Task 2 - Created Order Class

--- cc_10.js
+++ cc_10.js
@@ -32,4 +32,33 @@ console.log(prod1.getDetails());
 // Expected output: "Product: Laptop, ID: 101, Price: $1200, Stock: 7"
 
 
+//Task 2
+class Order {
+    constructor (orderID, product, quantity) {
+        if(product.stock >= quantity) {
+            this.orderID = orderID;
+            this.product = product;
+            this.quantity = quantity;
+            product.updateStock(quantity);
+        } else {
+            console.log ("Order cannot be placed due to insufficient stock");
+        }
+
+    }
+
+    getOrderDetails() {
+        this.totalPrice = this.product.price * this.quantity;
+        return `Order ID: ${this.orderID}, Product: ${this.product.name}, Quantity: ${this.quantity}, Total Price: $${this.totalPrice}`;
+    }
+}
+
+
+//Test Cases
+const order1 = new Order(501, prod1, 2);
+console.log(order1.getOrderDetails()); 
+// Expected output: "Order ID: 501, Product: Laptop, Quantity: 2, Total Price: $2400"
+
+console.log(prod1.getDetails()); 
+// Expected output: "Product: Laptop, ID: 101, Price: $1200, Stock: 5" (Stock reduced)
+
 
